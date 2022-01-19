bdab10c2559072d590eb4061fc134616e1dec971
Lucas Vilar <lucasdantasvilar@hotmail.com>
2022-01-19 03:03:31 -0300

    fixation exercise done

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -1,6 +1,27 @@
 require('../mocks/fetchSimulator');
+const character = require('../mocks/character');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
-describe('Teste a função fetchCharacter', () => {
-  it('', () => {});
+describe('testing fetchCharacter', () => {
+  it('wonder woman?', async () => {
+    const char = await fetchCharacter('720');
+    expect(char.name).toBe('Wonder Woman');
+  });
+
+  it('no parameter', async () => {
+    const fail = await fetchCharacter();
+    expect(fail).toEqual(new Error('You must provide an url'));
+  })
+
+  it('missing parameter', async () => {
+    const response = await fetchCharacter('parametro qualquer');
+    expect(response).toBe('Invalid id');
+  });
+
+  it('checks the endpoint of fetch', async () => {
+    const url = 'https://www.superheroapi.com/api.php/4192484924171229/720';
+    await fetchCharacter('720');
+    expect(fetch).toHaveBeenCalledTimes(4);
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
 });
